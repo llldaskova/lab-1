890a51357c45fe7bc576af666d22b700f3459a08
lll.daskova <lll.daskova@mail.ru>
2022-07-25 17:23:39 +0700

    c

---
+++ lab 1/HW1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class HW1
+    {
+        static int MinElement(int[] array)
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] < min)
+                    min = array[i];
+            return min;
+        }
+        static int MaxElement(int[] array)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] > max)
+                    max = array[i];
+            return max;
+        }
+        static void reduceQueue(int[] array,int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] -= element;
+        }
+        public static long QueueTime(int[] customers, int n)
+        {
+            int[] queues = new int[n];
+            int time=0;
+           /* for (int i = 0; i < queues.Length; i++)
+                queues[i] = customers[i];
+            int element1 = MinElement(queues);
+            reduceQueue(queues, element1);
+            time += element;*/
+            for (int i= 0; i<customers.Length;)
+            {
+                int element = MinElement(queues);
+                reduceQueue(queues, element);
+                for (int q = 0; q < queues.Length; q++)
+                    if(queues[q]==0)
+                    {
+                        queues[q] = customers[i];
+                        i++;
+                        //time += element;
+                    }
+                time += element;
+             /*  int index= MinElementIndex(queues);
+               reduceQueue(queues, index);
+               time += queues[index];
+                queues[index] = customers[i];*/
+            }
+            return time+ MaxElement(queues);
+        }
+    }
+}
---
+++ lab 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new[] { 5, 3, 4 };
+            Console.WriteLine(HW1.QueueTime(a, 1));
+            a = new[] { 10, 2, 3, 3 };
+            Console.WriteLine(HW1.QueueTime(a, 2));
+            a = new[] { 10, 2, 3 };
+            Console.WriteLine(HW1.QueueTime(a, 2));
+        }
+    }
+}
